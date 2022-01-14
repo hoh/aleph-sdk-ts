c6861c343aaf2feb6cc7cfed3748a3b6b751abe6
Roman Gascoin <54111474+Rgascoin@users.noreply.github.com>
2022-01-14 19:51:05 +0100

    Update Near sign method with Signer

--- src/accounts/near.ts
+++ src/accounts/near.ts
@@ -2,20 +2,30 @@ import { Account, ChainType } from "./account";
 import { BaseMessage, GetVerificationBuffer } from "../messages/message";
 import base58 from "bs58";
 import * as nearAPI from "near-api-js";
+import { ConnectConfig } from "near-api-js/lib/connect";
+import { InMemorySigner, KeyPair, keyStores } from "near-api-js";
+import { KeyStore } from "near-api-js/lib/key_stores";
+
+export type NearNet = "mainnet" | "betanet" | "testnet";
 
 /**
  * NEARAccount implements the Account class for the NEAR protocol.
  * It is used to represent an near account when publishing a message on the Aleph network.
  */
 export class NEARAccount extends Account {
-    private readonly wallet: nearAPI.KeyPair;
+    private readonly wallet: KeyStore;
+    private readonly accountId: string;
+    private readonly networkId: string;
 
-    constructor(wallet: nearAPI.KeyPair) {
-        const publicKey = wallet.getPublicKey().toString();
-        const address = base58.decode(publicKey.replace("ed25519:", "")).toString("hex");
+    constructor(wallet: KeyStore, keypair: KeyPair, accountId: string, networkId: string) {
+        const publicKey = keypair.getPublicKey().toString();
+        const address = publicKey.replace("ed25519:", "");
+        const accountAddress = accountId.concat(":", address);
 
-        super(address, publicKey);
+        super(accountAddress, publicKey);
         this.wallet = wallet;
+        this.accountId = accountId;
+        this.networkId = networkId;
     }
 
     override GetChain(): ChainType {
@@ -30,12 +40,13 @@ export class NEARAccount extends Account {
      *
      * @param message The Aleph message to sign, using some of its fields.
      */
-    override Sign(message: BaseMessage): Promise<string> {
+    override async Sign(message: BaseMessage): Promise<string> {
         const buffer = Buffer.from(GetVerificationBuffer(message));
 
-        return new Promise((resolve) => {
-            const signature = this.wallet.sign(buffer);
+        const signer = new InMemorySigner(this.wallet);
+        const signature = await signer.signMessage(buffer, this.accountId, this.networkId);
 
+        return new Promise(async (resolve) => {
             resolve(
                 JSON.stringify({
                     signature: base58.encode(signature.signature),
@@ -51,21 +62,40 @@ export class NEARAccount extends Account {
  *
  * It creates a NEAR wallet containing information about the account, extracted in the NEARAccount constructor.
  *
+ * @param network the network where you account belongs.
+ * @param accountId Your Near account id.
  * @param privateKey The private key of the account to import.
  */
-export function ImportAccountFromPrivateKey(privateKey: string): NEARAccount {
-    const { KeyPair } = nearAPI;
-
+export async function ImportAccountFromPrivateKey(
+    network: NearNet,
+    accountId: string,
+    privateKey: string,
+): Promise<NEARAccount> {
+    const { KeyPair, connect } = nearAPI;
+    let accountVerified = false;
+    const keyStore = new keyStores.InMemoryKeyStore();
     const keyPair = KeyPair.fromString(privateKey);
-    return new NEARAccount(keyPair);
-}
+    const publicKey = keyPair.getPublicKey().toString();
+    await keyStore.setKey(network, accountId, keyPair);
 
-/**
- * Creates a new NEAR account using the fromRandom method given by 'near-api-js'
- */
-export function NewAccount(): NEARAccount {
-    const { KeyPair } = nearAPI;
+    const config: ConnectConfig = {
+        headers: {},
+        keyStore: keyStore,
+        networkId: network,
+        nodeUrl: `https://rpc.${network}.near.org`,
+        walletUrl: `https://wallet.${network}.near.org`,
+        helperUrl: `https://helper.${network}.near.org`,
+    };
+    const near = await connect(config);
+    const account = await near.account(accountId);
+    const accessKeys = await account.getAccessKeys();
+    for (let i = 0; i < accessKeys.length; i++) {
+        if (accessKeys[i].public_key === publicKey) {
+            accountVerified = true;
+            break;
+        }
+    }
 
-    const keyPair = KeyPair.fromRandom("ed25519");
-    return new NEARAccount(keyPair);
+    if (!accountVerified) throw "Your Near account can't be verified";
+    return new NEARAccount(keyStore, keyPair, accountId, network);
 }
--- tests/accounts/near.test.ts
+++ tests/accounts/near.test.ts
@@ -2,29 +2,27 @@ import { BaseMessage, GetVerificationBuffer, MessageType, StorageEngine } from "
 import { ChainType } from "../../src/accounts/account";
 import { DEFAULT_API_V2 } from "../../src/global";
 import { aggregate, near } from "../index";
+import { sha256 } from "js-sha256";
 import nacl from "tweetnacl";
 import base58 from "bs58";
 
 describe("Near accounts", () => {
-    it("should import a random new account", () => {
-        const account = near.NewAccount();
-
-        expect(account.address).not.toBe("");
-        expect(account.publicKey).not.toBe("");
-    });
-
-    it("should import an account from a private key", () => {
-        const account = near.ImportAccountFromPrivateKey(
-            "32BgRyhTnsFFaQrsUz51KfaRu8B76Xr9EZEbib4rPgPEFdzn1oE9he9ZNj9yznGHrrH5NLZoMWEhvS1a3XtD23wf",
+    it("should import an account from a private key", async () => {
+        const account = await near.ImportAccountFromPrivateKey(
+            "testnet",
+            "thewinnie.testnet",
+            "5ZcDQZjegV4JBQjVkWzaehq8f3fviWVyRkN89JbjFenWFN5RgRnGr7QNYdfMmjfkgy4sg9pBu3bKSDRJFUeovgjn",
         );
 
-        expect(account.address).toBe("c344aff8c578f48955a26c58f9d380fc53a6e7b7483b0b6b01fdeb110c92a9f2");
-        expect(account.publicKey).toBe("ed25519:E9FFR5xh7naWkk5rMaVYhKWntWaA1ATEDXHTE1rePd13");
+        expect(account.address).toBe("thewinnie.testnet:7KRctNKvQbDFxT6QhvUHgznCqEJtQvQohzjsZ9fPrjaC");
+        expect(account.publicKey).toBe("ed25519:7KRctNKvQbDFxT6QhvUHgznCqEJtQvQohzjsZ9fPrjaC");
     });
 
     it("should sign a message with near account and verify it", async () => {
-        const account = near.ImportAccountFromPrivateKey(
-            "32BgRyhTnsFFaQrsUz51KfaRu8B76Xr9EZEbib4rPgPEFdzn1oE9he9ZNj9yznGHrrH5NLZoMWEhvS1a3XtD23wf",
+        const account = await near.ImportAccountFromPrivateKey(
+            "testnet",
+            "thewinnie.testnet",
+            "5ZcDQZjegV4JBQjVkWzaehq8f3fviWVyRkN89JbjFenWFN5RgRnGr7QNYdfMmjfkgy4sg9pBu3bKSDRJFUeovgjn",
         );
 
         const base: BaseMessage = {
@@ -44,17 +42,19 @@ describe("Near accounts", () => {
         const nearSigned = await account.Sign(base);
         const signatureParsed = JSON.parse(nearSigned);
         const valid = nacl.sign.detached.verify(
-            GetVerificationBuffer(base),
+            new Uint8Array(sha256.array(GetVerificationBuffer(base))),
             base58.decode(signatureParsed.signature),
-            Buffer.from(account.address, "hex"),
+            base58.decode(signatureParsed.publicKey),
         );
         expect(nearSigned).not.toBeNull();
         expect(valid).toBe(true);
     });
 
     it("should publish an aggregate message", async () => {
-        const account = near.ImportAccountFromPrivateKey(
-            "32BgRyhTnsFFaQrsUz51KfaRu8B76Xr9EZEbib4rPgPEFdzn1oE9he9ZNj9yznGHrrH5NLZoMWEhvS1a3XtD23wf",
+        const account = await near.ImportAccountFromPrivateKey(
+            "testnet",
+            "thewinnie.testnet",
+            "5ZcDQZjegV4JBQjVkWzaehq8f3fviWVyRkN89JbjFenWFN5RgRnGr7QNYdfMmjfkgy4sg9pBu3bKSDRJFUeovgjn",
         );
         const key = "neeaarr";
 
